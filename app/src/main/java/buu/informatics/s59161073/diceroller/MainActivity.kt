e2fbc6c9b2df59cfdf7fd39601be23ffff2e0b58
mmineejuta27 <minejutamas@gmail.com>
2019-08-07 18:35:39 +0700

    Task: Add and update image resources

--- app/src/main/java/buu/informatics/s59161073/diceroller/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59161073/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package buu.informatics.s59161073.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -16,8 +17,6 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = "Dice Rolled!"
 
         val resetButton: Button = findViewById(R.id.reset_button)
         resetButton.setOnClickListener { resetDice() }
@@ -25,14 +24,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun resetDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = "0".toString()
+
     }
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
-        Toast.makeText(this,"button clicked",Toast.LENGTH_SHORT).show()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
